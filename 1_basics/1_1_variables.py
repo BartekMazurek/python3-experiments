1d7bb10c33d4c21c8aed059df7b1d0930c34f722
BartekMazurek <bartlomiejmazurek9@gmail.com>
2022-05-27 18:35:20 +0200

    Added basic variables script

---
+++ 1_basics/1_1_variables.py
@@ -0,0 +1,22 @@
+# VARIABLES (SIMPLE TYPES)
+
+# INTEGER
+integerValue = 100
+
+# FLOAT
+floatValue = 10.50
+
+# STRING
+stringValue = '{"name":"Test", "age": 30}'
+
+# BOOLEAN
+booleanValue = False
+
+# ASSIGN MULTIPLE VARIABLES
+firstVariable = 1; secondVariable = 3.5; thirdVariable = 'Test'
+
+# ASSIGN MULTIPLE VARIABLES - ALTERNATIVE
+fourthVariable, fifthVariable, sixthVariable = 1, 3.5, 'Test'
+
+# CHECK VARIABLE TYPE
+# print(type(stringValue))
