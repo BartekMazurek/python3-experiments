48534fc8115cc0ff877a655f2875dc4a8726042f
BartekMazurek <bartlomiejmazurek9@gmail.com>
2022-05-27 18:35:32 +0200

    Added basic operators script

---
+++ 1_basics/1_2_operators.py
@@ -0,0 +1,50 @@
+# MATH OPERATORS
+
+# ADD
+first = 1
+second = 3
+
+firstResult = first + second
+print('Add', firstResult)
+
+# SUBTRACT
+third = 2
+fourth = 2
+
+secondResult = third - fourth
+print('Subtract', secondResult)
+
+# MULTIPLY
+fifth = 5
+sixth = 5
+
+thirdResult = fifth * sixth
+print('Multiply', thirdResult)
+
+# DIVIDE
+seventh = 10
+eighth = 5
+
+fourthResult = 10 / 5
+print('Divide', fourthResult)
+
+# POWER
+ninth = 2
+tenth = 3
+
+fifthResult = ninth ** tenth
+print('Power', fifthResult)
+
+# DIVIDE "FLOOR"
+eleventh = 9
+twelfth = 4
+
+sixthResult = eleventh // twelfth
+print('Divide floor', sixthResult)
+
+# MODULO
+thirteenth = 4
+fourteenth = 3
+
+seventhResult = thirteenth % fourteenth
+print('Modulo', seventhResult)
