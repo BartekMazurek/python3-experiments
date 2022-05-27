3574e9230a6aa510d43f482ccc7092b6da385716
BartekMazurek <bartlomiejmazurek9@gmail.com>
2022-05-27 18:35:44 +0200

    Added basic strings script

---
+++ 1_basics/1_3_strings.py
@@ -0,0 +1,18 @@
+# OPERATIONS ON STRING
+
+testString = 'Some random text'
+
+# PRINT FIRST LETTER
+print('First letter: ', testString[0])
+
+# PRINT LAST LETTER
+print('Last letter: ', testString[-1])
+
+# PRINT WHOLE TEXT - : OPERATOR
+print('Print whole text: ', testString[:])
+
+# PRINT WHOLE TEXT WITHOUT FIRST LETTER
+print('Print text again without first letter: ', testString[1:])
+
+# PRINT WHOLE TEXT WITHOUT LAST LETTER
+print('Print text again without last letter: ', testString[:-1])
