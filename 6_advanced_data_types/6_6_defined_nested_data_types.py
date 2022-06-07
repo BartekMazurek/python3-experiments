f802770e4a8c025deeab3d89de6eeec1c6fcc98d
BartekMazurek <bartlomiejmazurek9@gmail.com>
2022-06-07 20:57:51 +0200

    Fixed formatting - compared with PEP8

--- 6_advanced_data_types/6_6_defined_nested_data_types.py
+++ 6_advanced_data_types/6_6_defined_nested_data_types.py
@@ -2,47 +2,45 @@
 
 try:
 
-    definitions = {
-
-    }
+    definitions = {}
 
     while True:
-        print('1: Add definition')
-        print('2: Find definition')
-        print('3: Delete definition')
-        print('4: Exit')
+        print("1: Add definition")
+        print("2: Find definition")
+        print("3: Delete definition")
+        print("4: Exit")
 
-        operationValue = int(input('Choose operation: '))
+        operation_value = int(input("Choose operation: "))
 
-        if operationValue == 1:
-            keyName = input('Enter key name: ')
-            definition = input('Enter key value definition: ')
-            definitions[keyName] = definition
-            print('Added new definition')
+        if operation_value == 1:
+            key_name = input("Enter key name: ")
+            definition = input("Enter key value definition: ")
+            definitions[key_name] = definition
+            print("Added new definition")
 
-        elif operationValue == 2:
-            keyToFind = input('Enter key name to find: ')
+        elif operation_value == 2:
+            key_to_find = input("Enter key name to find: ")
 
-            if keyToFind in definitions:
-                print(definitions[keyToFind])
+            if key_to_find in definitions:
+                print(definitions[key_to_find])
             else:
-                print('Specified key does not exist in definitions')
+                print("Specified key does not exist in definitions")
 
-        elif operationValue == 3:
-            keyToDelete = input('Enter key name to delete: ')
+        elif operation_value == 3:
+            key_to_delete = input("Enter key name to delete: ")
 
-            if keyToDelete in definitions:
-                del definitions[keyToDelete]
+            if key_to_delete in definitions:
+                del definitions[key_to_delete]
             else:
-                print('Specified key does not exist in definitions')
+                print("Specified key does not exist in definitions")
 
-        elif operationValue == 4:
-            print('Exited')
+        elif operation_value == 4:
+            print("Exited")
 
             break
 
         else:
-            print('Wrong operation number. Exited')
+            print("Wrong operation number. Exited")
 
             break
 except ValueError:
