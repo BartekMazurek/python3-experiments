8b2bdc9c93139ef6a26d4f8b8ecc12601b2f8508
BartekMazurek <bartlomiejmazurek9@gmail.com>
2022-06-20 18:26:21 +0200

    Added simple function example

---
+++ 8_advanced_functions/8_1_advanced_functions.py
@@ -0,0 +1,11 @@
+# CREATING FUNCTIONS
+# USE def TO CREATE FUNCTION
+
+def some_function_name(argument_name):
+    print('Doing some stuff with argument ...', argument_name)
+
+
+arguments = ['arg1', 'arg2', 'arg3']
+
+for argument in arguments:
+    some_function_name(argument)
