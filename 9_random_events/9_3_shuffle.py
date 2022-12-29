3067cd4fa7e470c3daa11de0eaaf7da3c2034d81
BartekMazurek <bartlomiejmazurek9@gmail.com>
2022-12-29 08:58:32 +0100

    Added list shuffle example

---
+++ 9_random_events/9_3_shuffle.py
@@ -0,0 +1,8 @@
+# SHUFFLE ELEMENTS ON LIST
+import random
+
+elements = ["a", "b", "c", "d", "e", "f", "g"]
+
+# CHANGE ELEMENTS ORDER ON LIST
+random.shuffle(elements)
+print('Shuffled elements: ', elements)
