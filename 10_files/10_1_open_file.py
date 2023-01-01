7acb257f555efb3337d2efb61ddb1daaec3505aa
BartekMazurek <bartlomiejmazurek9@gmail.com>
2023-01-01 22:21:42 +0100

    File handler example

---
+++ 10_files/10_1_open_file.py
@@ -0,0 +1,12 @@
+# OPEN FILE
+# OPERATIONS: OPEN, WRITE/READ, CLOSE
+# MODES: (r)ead, (w)rite, (c)lose
+
+# OPEN FILE HANDLE
+file = open("file.txt", "w")
+
+# WRITE CONTENT INTO FILE
+file.write('Some new content')
+
+# CLOSE FILE HANDLE
+file.close()
